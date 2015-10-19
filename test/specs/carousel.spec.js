01b9d15b245830e106b0e07bb6f0412c29f4d3ee
trashgenerator <trashgenerator@gmail.com>
2015-10-19 13:57:54 +0300

    Change tests to use 'react-addons-test-utils' instead of 'react/addons'
    Because 'react/addons' is deprecated in React 0.14

--- test/specs/carousel.spec.js
+++ test/specs/carousel.spec.js
@@ -2,8 +2,8 @@
 
 describe('Carousel', function () {
 
-  var React = require('react/addons');
-  var TestUtils = React.addons.TestUtils;
+  var React = require('react');
+  var TestUtils = require('react-addons-test-utils');
 
   var carousel, container, component;
 
