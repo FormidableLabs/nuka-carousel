d0f42ba9aface5c7e08c80516e324229c44e80cc
Andreas Eldh <eldh@eldh.local>
2015-11-02 15:12:33 +0100

    Take cell spacing into account during touchMove
    Fix #34.
    
    Add cellSpacing to the top/left calculations in the onTouchMove handler.

--- src/carousel.js
+++ src/carousel.js
@@ -191,8 +191,8 @@ const Carousel = React.createClass({
         }
 
         self.setState({
-          left: self.props.vertical ? 0 : (self.state.slideWidth * self.state.currentSlide + (self.touchObject.length * self.touchObject.direction)) * -1,
-          top: self.props.vertical ? (self.state.slideWidth * self.state.currentSlide + (self.touchObject.length * self.touchObject.direction)) * -1 : 0
+          left: self.props.vertical ? 0 : ((self.state.slideWidth + self.props.cellSpacing) * self.state.currentSlide + (self.touchObject.length * self.touchObject.direction)) * -1,
+          top: self.props.vertical ? ((self.state.slideWidth + self.props.cellSpacing) * self.state.currentSlide + (self.touchObject.length * self.touchObject.direction)) * -1 : 0
         });
       },
       onTouchEnd(e) {
