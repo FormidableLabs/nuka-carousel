826fbd9745eb10b9f0af121c4be28b67b6d9bda1
Ivan Jager <ivan.jager@wicourts.gov>
2016-06-30 10:07:08 -0500

    Cleanup and add more examples to demo

--- demo/app.js
+++ demo/app.js
@@ -41,7 +41,7 @@ const makeChildren = n => {
   var children = [];
   for (let i = 0; i < n; i++) {
     children.push(
-      <img key={i} src={`http://placehold.it/1000x400&text=slide${i + 1}`}/>
+      <img key={i} src={`http://placehold.it/1000x400&text=Slide%20${i + 1}`}/>
     );
   }
   return children;
@@ -49,17 +49,15 @@ const makeChildren = n => {
 
 
 const LazyCarousel = ({count, ...props}) => {
-  // TODO: Is there a cleaner way to hide the dots?
-  const noDots = Carousel.getDefaultProps().decorators.slice(0, 2);
   return (
-    <Carousel
-      {...props}
-      decorators={noDots}
-      lazyLoad={true}>
+    <Carousel {...props} lazyLoad={true}>
       {makeChildren(count)}
     </Carousel>
   );
 };
+
+// TODO: Is there a cleaner way to hide the dots?
+const noDots = Carousel.getDefaultProps().decorators.slice(0, 2);
 
 const App = () => (
   <div style={{width: '50%', margin: 'auto'}}>
@@ -67,10 +65,16 @@ const App = () => (
     <ControlledCarousel/>
 
     <h1>Lazy loading</h1>
-    <LazyCarousel count={1000}/>
+    <LazyCarousel count={1000} decorators={noDots}/>
 
     <h1>Multiple slides</h1>
     <LazyCarousel count={10} slidesToShow={4} slidesToScroll='auto'/>
+
+    <h1>Center mode</h1>
+    <LazyCarousel count={10} slidesToShow={4} slidesToScroll='auto' cellAlign='center'/>
+
+    <h1>Disable dragging</h1>
+    <LazyCarousel count={6} dragging={false}/>
   </div>
 );
 
