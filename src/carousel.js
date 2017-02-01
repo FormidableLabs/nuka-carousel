344dd2a88435c6ea942408cef64989e7b4821094
Ritwik Roy <me@ritwikroy.com>
2017-01-31 23:46:48 -0800

    Modify clickSafe default.
    When both swiping and dragging are disabled, clickSafe isn't needed
    yet it is set to true by default because the handleSwipe function
    is never called to unset it after a click. This means the click event is
    always blocked.
    
    This change replaces that behaviour by defaulting clickSafe to false and
    enabling it if either dragging or swiping.

--- src/carousel.js
+++ src/carousel.js
@@ -214,6 +214,8 @@ const Carousel = React.createClass({
 
     if (self.props.swiping === false) {
       return null;
+    } else {
+      self.clickSafe = true;
     }
 
     return {
@@ -263,13 +265,15 @@ const Carousel = React.createClass({
     }
   },
 
-  clickSafe: true,
+  clickSafe: false,
 
   getMouseEvents() {
     var self = this;
 
     if (this.props.dragging === false) {
       return null;
+    } else {
+      self.clickSafe = true;
     }
 
     return {
