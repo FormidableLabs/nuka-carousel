5bbecd7b50617b5839889d377eedb1775fe4e086
Anton Kosiak <Keleborn.mail@gmail.com>
2017-09-12 17:29:50 +0300

    Fix Render <li> element even one child exists
    Fixed Render <li> element even one child exists
    Resolving Issue #234
    https://github.com/FormidableLabs/nuka-carousel/issues/234

--- src/carousel.js
+++ src/carousel.js
@@ -166,10 +166,7 @@ const Carousel = createReactClass({
 
   render() {
     var self = this;
-    var children =
-      React.Children.count(this.props.children) > 1
-        ? this.formatChildren(this.props.children)
-        : this.props.children;
+    var children = this.formatChildren(this.props.children);
     return (
       <div
         className={['slider', this.props.className || ''].join(' ')}
