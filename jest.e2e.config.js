4eae4e0a60f9ce2694ba02d6d4b6fbf31346c017
Elrey Belmonti <elrey.belmonti@formidable.com>
2018-10-17 15:34:15 -0700

    remove testURL from test files

--- jest.e2e.config.js
+++ jest.e2e.config.js
@@ -1,5 +1,4 @@
 module.exports = {
   preset: 'jest-puppeteer-preset',
-  roots: ['<rootDir>/test/e2e/'],
-  testURL: 'http://localhost:3000'
+  roots: ['<rootDir>/test/e2e/']
 };
--- jest.unit.config.js
+++ jest.unit.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   testEnvironment: 'enzyme',
   setupTestFrameworkScriptFile: './node_modules/jest-enzyme/lib/index.js',
-  roots: ['<rootDir>/test/specs/'],
-  testURL: 'http://localhost:3000'
+  roots: ['<rootDir>/test/specs/']
 };
