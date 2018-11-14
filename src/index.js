5c03a006d65c1cb5c3a3e96a910b1b47ffe1c566
Jack Tomaszewski <44373826+jack-sf@users.noreply.github.com>
2018-11-14 23:10:24 +0100

    listen to mouse hover effects (to disable autoplay) even when dragging/swiping is disabled (#439)

--- src/index.js
+++ src/index.js
@@ -166,7 +166,14 @@ export default class Carousel extends React.Component {
 
   getTouchEvents() {
     if (this.props.swiping === false) {
-      return null;
+      return {
+        onTouchStart: () => {
+          this.handleMouseOver();
+        },
+        onTouchEnd: () => {
+          this.handleMouseOut();
+        }
+      };
     }
 
     return {
@@ -236,7 +243,11 @@ export default class Carousel extends React.Component {
 
   getMouseEvents() {
     if (this.props.dragging === false) {
-      return null;
+      return {
+        onMouseOver: () => this.handleMouseOver(),
+
+        onMouseOut: () => this.handleMouseOut()
+      };
     }
 
     return {
