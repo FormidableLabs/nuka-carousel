48a6bfeb43f15320cd2da09f8bce0d3e0b0fb3ea
Ashika Kasiviswanathan <ashika.kasiviswanathan@formidable.com>
2019-04-18 14:44:26 -0700

    hooks demo

--- src/new-hooks/index.js
+++ src/new-hooks/index.js
@@ -1,20 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
+import Slide from './slide';
 
 /* eslint-disable func-style */
 function Example() {
-  const [count, setCount] = useState(0);
-
-  // Similar to componentDidMount and componentDidUpdate:
-  useEffect(() => {
-    // Update the document title using the browser API
-    document.title = `You clicked ${count} times`;
-  });
+  const [count, setCount] = useState(1);
 
   return (
     <div>
-      <p>You clicked {count} times</p>
-      <button onClick={() => setCount(count + 1)}>Click me</button>
+      <Slide>
+        <h1>Slide {count}</h1>
+      </Slide>
+      <button onClick={() => setCount(count - 1)}>prev</button>
+      <button onClick={() => setCount(count + 1)}>next</button>
     </div>
   );
 }
