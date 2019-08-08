5f980f1d9ba070fee6912c9fef673a82e76b7f73
Andrew Hutchings <andrew.hutchings@gmail.com>
2019-08-07 21:51:39 -0400

    Restore index.js file for backwards and default CJS compatibility

---
+++ index.js
@@ -0,0 +1,5 @@
+module.exports = require('./lib').default;
+module.exports.default = module.exports;
+module.exports.NextButton = require('./lib').NextButton;
+module.exports.PreviousButton = require('./lib').PreviousButton;
+module.exports.PagingDots = require('./lib').PagingDots;
