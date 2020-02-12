ceb917ee4d867f97c67a6be690b7b190f77a5f51
Sarah Egan <sarah.meyer@formidable.com>
2020-02-12 12:38:48 -0700

    Revert  (#665)
    * Revert "Issue 622: Updated tab logic so slides that are not fully visible cannot receive focus"
    This reverts commit 9b4348609ddb779751be2cc5bda4c6fabf86c19a.
    * rename getSlideHeight method

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import 'wicg-inert';
 import PropTypes from 'prop-types';
 import ExecutionEnvironment from 'exenv';
 import Animate from 'react-move/Animate';
@@ -1061,7 +1060,6 @@ export default class Carousel extends React.Component {
       });
     }
   }
-
   render() {
     const { currentSlide, slideCount, frameWidth } = this.state;
     const {
