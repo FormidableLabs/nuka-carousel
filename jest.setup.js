b967633a53a69024277a66c89f6287f5e9b763c2
Sarah Meyer <sarah.meyer@formidable.com>
2021-05-07 16:19:01 -0600

    fix: update jest config with react 17 adapter

---
+++ jest.setup.js
@@ -0,0 +1,4 @@
+import Enzyme from 'enzyme';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+
+Enzyme.configure({ adapter: new Adapter() });
--- jest.unit.config.js
+++ jest.unit.config.js
@@ -1,5 +1,8 @@
 module.exports = {
   testEnvironment: 'enzyme',
-  setupFilesAfterEnv: ['./node_modules/jest-enzyme/lib/index.js'],
+  setupFilesAfterEnv: [
+    './node_modules/jest-enzyme/lib/index.js',
+    '<rootDir>/jest.setup.js'
+  ],
   roots: ['<rootDir>/test/specs/']
 };
