a0c36848dfe849498753faafc8d704f7d122a110
Jacob Groß <kurtextrem@gmail.com>
2021-08-18 17:16:48 +0200

    Use more modern screen-reader only styles
    See: https://a11y-guidelines.orange.com/en/web/components-examples/accessible-hiding/. 
    
    This also improves performance, since the previously used `left: -1000px` technique created huge layers when the div was close to (or overlapped) an element which got promoted to its own layer.

--- src/announce-slide.js
+++ src/announce-slide.js
@@ -3,11 +3,14 @@ import React from 'react';
 const AnnounceSlide = ({ message }) => {
   const styles = {
     position: 'absolute',
-    left: '-10000px',
-    top: 'auto',
     width: '1px',
     height: '1px',
-    overflow: 'hidden'
+    overflow: 'hidden',
+    padding: 0,
+    margin: '-1px',
+    clip: 'rect(0, 0, 0, 0)',
+    'white-space': 'nowrap',
+    border: 0,
   };
   return (
     <div aria-live="polite" aria-atomic="true" style={styles} tabIndex={-1}>
