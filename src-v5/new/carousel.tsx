fb7898211925b3f163ed3edfff10dde2d2015334
Valentin Georgiev <valentin.al.georgiev@gmail.com>
2021-12-21 11:26:17 +0200

    v5 migrate autoplay and ref

--- src-v5/new/carousel.tsx
+++ src-v5/new/carousel.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState, useRef, MutableRefObject } from 'react';
 import Slide from './slide';
 import { getSliderListStyles } from './slider-list';
 import { CarouselProps, Directions } from './types';
@@ -8,18 +8,61 @@ import defaultProps from './default-carousel-props';
 const Carousel = (props: CarouselProps): React.ReactElement => {
   const [currentSlide, setCurrentSlide] = useState<number>(0);
   const [direction, setDirection] = useState<Directions | null>(null);
+  const [pause, setPause] = useState<boolean>(false);
+  const count = React.Children.count(props.children);
+  const timer: MutableRefObject<ReturnType<typeof setTimeout> | null> = useRef(null)
 
   const nextSlide = () => {
-    setDirection(Directions.Next);
-    setCurrentSlide(currentSlide + 1);
+    // boundary
+    if (currentSlide < count - props.slidesToShow) {
+      setDirection(Directions.Next);
+      setCurrentSlide(currentSlide + 1);
+    }
   };
 
   const prevSlide = () => {
-    setDirection(Directions.Prev);
-    setCurrentSlide(currentSlide - 1);
+    // boundary
+    if (currentSlide > 0) {
+      setDirection(Directions.Prev);
+      setCurrentSlide(currentSlide - 1);
+    }
   };
 
-  const count = React.Children.count(props.children);
+  useEffect(() => {
+
+    if (
+      props.autoplay &&
+      currentSlide >= 0 &&
+      currentSlide < count - props.slidesToShow &&
+      !pause
+    ) {
+      timer.current = setTimeout(() => {
+        if (props.autoplayReverse) {
+          prevSlide();
+        } else {
+          nextSlide();
+        }
+      }, props.autoplayInterval);
+    }
+
+    // Clear the timeout if user hover on carousel
+    if (props.autoplay && pause && timer?.current) {
+      clearTimeout(timer.current)
+    }
+  }, [currentSlide, pause]);
+
+  const onMouseEnter = () => {
+    if (props.pauseOnHover) {
+      setPause(true)
+    }
+  }
+
+  const onMouseLeave = () => {
+    if (props.pauseOnHover) {
+      setPause(false)
+    }
+  }
+
   const slides = React.Children.map(props.children, (child, index) => (
     <Slide key={index} count={count}>
       {child}
@@ -28,13 +71,16 @@ const Carousel = (props: CarouselProps): React.ReactElement => {
 
   return (
     <div
-      className="slider-frame"
+      className={['slider-frame', props.className || ''].join(' ').trim()}
       style={{
         overflow: 'hidden',
         width: '100%',
         position: 'relative',
         ...props.style
       }}
+      ref={props.innerRef}
+      onMouseEnter={onMouseEnter}
+      onMouseLeave={onMouseLeave}
     >
       <div
         className="slider-list"
