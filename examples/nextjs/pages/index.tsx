8589155eba1316b6d9aece5259a39ce66f44d30f
Valentin Georgiev <valentin.al.georgiev@gmail.com>
2022-01-20 13:38:48 +0200

    v5: setup cypress and write some basic tests

--- examples/nextjs/pages/index.tsx
+++ examples/nextjs/pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 import Carousel from '../../../src-v5/new/carousel';
+import { useRouter } from 'next/router'
 
 const colors = [
   '7732bb',
@@ -14,20 +15,27 @@ const colors = [
   'aa231f'
 ];
 
-const Home = () => {
-  const slides = colors.map((color, index) => (
+const Home = ({ urlParams }) => {
+
+  const colorsArray = colors.slice(0, Number(urlParams.slides || 9))
+  
+  const slides = colorsArray.map((color, index) => (
     <img
       src={`https://via.placeholder.com/400/${color}/ffffff/&text=slide${
         index + 1
       }`}
       alt={`Slide ${index + 1}`}
       key={color}
+      data-slide={`Slide ${index + 1}`}
       style={{
         width: '100%',
         height: 400
       }}
     />
   ));
+
+
+  const carouselParams = urlParams.params ? JSON.parse(urlParams.params) : {}
 
   return (
     <div className={styles.container}>
@@ -42,12 +50,22 @@ const Home = () => {
 
       <main className={styles.main}>
         <h1>Nuka Carousel - SSR Example Formidable Labs</h1>
-        <Carousel slidesToShow={3} wrapAround>
+        <Carousel {...carouselParams}>
           {slides}
         </Carousel>
       </main>
     </div>
   );
 };
+
+export async function getServerSideProps(context) {
+  
+
+  return {
+    props: {
+      urlParams: context.query
+    },
+  }
+}
 
 export default Home;
