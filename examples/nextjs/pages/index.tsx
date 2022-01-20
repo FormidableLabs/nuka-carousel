1739149a5271af8343c17057757242d9ef02def9
Valentin Georgiev <valentin.al.georgiev@gmail.com>
2022-01-20 14:59:05 +0200

    fix not used module

--- examples/nextjs/pages/index.tsx
+++ examples/nextjs/pages/index.tsx
@@ -1,7 +1,6 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 import Carousel from '../../../src-v5/new/carousel';
-import { useRouter } from 'next/router'
 
 const colors = [
   '7732bb',
