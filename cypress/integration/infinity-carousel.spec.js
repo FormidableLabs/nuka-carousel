625779b11567c48c9f91418bcb26a58893ef4201
Valentin Georgiev <valentin.al.georgiev@gmail.com>
2022-01-27 16:24:59 +0200

    add e2e test

--- cypress/integration/infinity-carousel.spec.js
+++ cypress/integration/infinity-carousel.spec.js
@@ -454,6 +454,105 @@ context('Infinity Carousel', () => {
         .should('have.attr', 'data-slide', 'Slide 4');
     });
 
+    it('should render infinity carousel with 4 slides and all of them should be visible and go through all of the slides', () => {
+      const params = {
+        slidesToShow: 4,
+        wrapAround: true
+      };
+
+      cy.visit(
+        `http://localhost:3000/?slides=4&params=${JSON.stringify(params)}`
+      );
+
+      cy.get('.slide.slide-visible')
+        .should('have.length', 4)
+        .find('img')
+        .first()
+        .should('have.attr', 'data-slide', 'Slide 1');
+
+      cy.get('.paging-item').should('have.length', 1);
+
+      cy.get('button').contains('Prev').should('not.be.disabled');
+      cy.get('button').contains('Next').should('not.be.disabled').click();
+      cy.wait(500);
+
+      cy.get('.slide.slide-visible')
+        .should('have.length', 4)
+        .find('img')
+        .first()
+        .should('have.attr', 'data-slide', 'Slide 2');
+
+      cy.get('button').contains('Prev').should('not.be.disabled');
+      cy.get('button').contains('Next').should('not.be.disabled').click();
+      cy.wait(500);
+
+      cy.get('.slide.slide-visible')
+        .should('have.length', 4)
+        .find('img')
+        .first()
+        .should('have.attr', 'data-slide', 'Slide 3');
+
+      cy.get('button').contains('Prev').should('not.be.disabled');
+      cy.get('button').contains('Next').should('not.be.disabled').click();
+      cy.wait(500);
+
+      cy.get('.slide.slide-visible')
+        .should('have.length', 4)
+        .find('img')
+        .first()
+        .should('have.attr', 'data-slide', 'Slide 4');
+
+      cy.get('button').contains('Prev').should('not.be.disabled');
+      cy.get('button').contains('Next').should('not.be.disabled').click();
+      cy.wait(500);
+
+      cy.get('.slide.slide-visible')
+        .should('have.length', 4)
+        .find('img')
+        .first()
+        .should('have.attr', 'data-slide', 'Slide 1');
+
+      cy.get('button').contains('Prev').should('not.be.disabled');
+      cy.get('button').contains('Next').should('not.be.disabled').click();
+      cy.wait(500);
+
+      cy.get('.slide.slide-visible')
+        .should('have.length', 4)
+        .find('img')
+        .first()
+        .should('have.attr', 'data-slide', 'Slide 2');
+
+      cy.get('button').contains('Prev').should('not.be.disabled');
+      cy.get('button').contains('Next').should('not.be.disabled').click();
+      cy.wait(500);
+
+      cy.get('.slide.slide-visible')
+        .should('have.length', 4)
+        .find('img')
+        .first()
+        .should('have.attr', 'data-slide', 'Slide 3');
+
+      cy.get('button').contains('Prev').should('not.be.disabled');
+      cy.get('button').contains('Next').should('not.be.disabled').click();
+      cy.wait(500);
+
+      cy.get('.slide.slide-visible')
+        .should('have.length', 4)
+        .find('img')
+        .first()
+        .should('have.attr', 'data-slide', 'Slide 4');
+
+      cy.get('button').contains('Prev').should('not.be.disabled');
+      cy.get('button').contains('Next').should('not.be.disabled').click();
+      cy.wait(500);
+
+      cy.get('.slide.slide-visible')
+        .should('have.length', 4)
+        .find('img')
+        .first()
+        .should('have.attr', 'data-slide', 'Slide 1');
+    });
+
     it('should render infinity carousel with 5 slides and 2 visible slides without controls', () => {
       const params = {
         slidesToShow: 2,
