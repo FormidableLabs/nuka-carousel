ffc0eb8dcc85cf29db6715a00c6c9482be66fce8
Valentin Georgiev <valentin.al.georgiev@gmail.com>
2022-02-15 15:23:01 +0200

    chore: remove old version export

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
-module.exports = require('./lib').default;
+module.exports = require('./lib-v5').default;
 module.exports.default = module.exports;
-module.exports.NextButton = require('./lib').NextButton;
-module.exports.PreviousButton = require('./lib').PreviousButton;
-module.exports.PagingDots = require('./lib').PagingDots;
-
-module.exports.v5 = require('./lib-v5').default;
-module.exports.v5.NextButton = require('./lib-v5').NextButton;
-module.exports.v5.PreviousButton = require('./lib-v5').PreviousButton;
-module.exports.v5.PagingDots = require('./lib-v5').PagingDots;
+module.exports.NextButton = require('./lib-v5').NextButton;
+module.exports.PreviousButton = require('./lib-v5').PreviousButton;
+module.exports.PagingDots = require('./lib-v5').PagingDots;
