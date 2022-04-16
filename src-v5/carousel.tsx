e82fab00261641714264db7a5db4863e820e3287
Valentin Georgiev <valentin.al.georgiev@gmail.com>
2022-04-16 19:17:26 +0300

    fix: slide index property (#885)
    * fix: slide index property
    
    * fix: autoplay reverse

--- src-v5/carousel.tsx
+++ src-v5/carousel.tsx
@@ -121,6 +121,12 @@ export const Carousel = (props: CarouselProps): React.ReactElement => {
   };
 
   useEffect(() => {
+    if (typeof props.slideIndex === 'number' && !props.autoplayReverse) {
+      moveSlide(props.slideIndex);
+    }
+  }, [props.slideIndex]);
+
+  useEffect(() => {
     if (props.autoplay && !animation && props.wrapAround) {
       if (currentSlide > count) {
         setCurrentSlide(currentSlide - count);
