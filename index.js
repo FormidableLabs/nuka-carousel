67496ebc564560a72e15cd5497f07665f32edaa4
Harry Yu <hy.harry.yu@gmail.com>
2022-05-30 18:08:40 -0700

    fix: Use lib-v5/index.js instead of index.js

--- index.js
+++
@@ -1,5 +0,0 @@
-module.exports = require('./lib-v5').default;
-module.exports.default = module.exports;
-module.exports.NextButton = require('./lib-v5').NextButton;
-module.exports.PreviousButton = require('./lib-v5').PreviousButton;
-module.exports.PagingDots = require('./lib-v5').PagingDots;
