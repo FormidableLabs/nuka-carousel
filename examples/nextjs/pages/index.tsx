0c9dfdd3a8290b1b179037c2ace13921a84ec5ee
fritz-c <4413963+fritz-c@users.noreply.github.com>
2022-08-03 16:38:03 -0500

    Revert "Use inline image src for demo (#949)"
    This reverts commit ab95956d69ef85d56ce95f78cad232089ece678f.

--- examples/nextjs/pages/index.tsx
+++ examples/nextjs/pages/index.tsx
@@ -4,28 +4,42 @@ import styles from '../styles/Home.module.css';
 import Carousel from 'nuka-carousel';
 import Image from 'next/image';
 import { GetServerSideProps } from 'next';
-import { slideImages } from '../src/slideImages';
+
+const colors = [
+  '7732bb',
+  '047cc0',
+  '00884b',
+  'e3bc13',
+  'db7c00',
+  'aa231f',
+  'e3ac4a',
+  'db7c3e',
+  'ab23ff',
+  'ccc',
+  'ddd',
+  '000',
+  '111',
+  '222'
+];
 
 type HomeProps = {
   urlParams: Record<string, string>;
 };
-
 const Home = ({ urlParams }: HomeProps) => {
-  const slides = slideImages
-    .slice(0, Number(urlParams.slides || 9))
-    .map((slideImage, index) => (
-      // eslint-disable-next-line @next/next/no-img-element
-      <img
-        src={slideImage}
-        key={index}
-        alt={`Slide ${index + 1}`}
-        data-slide={`Slide ${index + 1}`}
-        style={{
-          width: '100%',
-          height: 400
-        }}
-      />
-    ));
+  const colorsArray = colors.slice(0, Number(urlParams.slides || 9));
+
+  const slides = colorsArray.map((color, index) => (
+    <img
+      src={`/images/${color}.png`}
+      key={color}
+      alt={`Slide ${index + 1}`}
+      data-slide={`Slide ${index + 1}`}
+      style={{
+        width: '100%',
+        height: 400
+      }}
+    />
+  ));
 
   const carouselParams = urlParams.params ? JSON.parse(urlParams.params) : {};
 
