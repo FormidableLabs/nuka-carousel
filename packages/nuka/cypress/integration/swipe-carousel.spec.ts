5861c20fffff6b322ef1c8daea63a9e8af2a345f
fritz-c <4413963+fritz-c@users.noreply.github.com>
2022-08-16 09:50:50 -0500

    test: use a couple utils in the swipe tests

--- packages/nuka/cypress/integration/swipe-carousel.spec.ts
+++ packages/nuka/cypress/integration/swipe-carousel.spec.ts
@@ -5,118 +5,99 @@
 // Swipe before and after threshold
 
 context('Swipe Carousel', () => {
-  // Speed up transition animation time
-  const sharedParams = { speed: 50 };
+  const carouselWidth = 600;
+  // Speed up transition animation time and set fixed width for easier drag
+  // distance calculations
+  const sharedParams = { speed: 50, style: { width: carouselWidth } };
+
+  const swipeSlider = (
+    distance,
+    { waitMs = 120 }: { waitMs?: number } = {}
+  ) => {
+    const [start, end] =
+      distance >= 0 ? [distance, 0] : [0, Math.abs(distance)];
+
+    cy.get('.slider-container')
+      .trigger('mousedown', { which: 1 })
+      .trigger('mousemove', { clientX: start });
+
+    cy.wait(waitMs);
+
+    cy.get('.slider-container')
+      .trigger('mousemove', { clientX: end })
+      .trigger('mouseup');
+  };
+  const assertFirstSlideIs = (slideNumber: number) => {
+    cy.get('.slide.slide-visible img').should(
+      'have.attr',
+      'data-slide',
+      `Slide ${slideNumber}`
+    );
+  };
 
   describe('Swipe Carousel variations', () => {
-    it('should render a carousel with 6 slides and 3 visible slides and go through all of the slides by swiping forwards and then backwards', () => {
+    it('should render a carousel with 6 slides and 3 visible slides and go through all of the slides by swiping forwards and then reverses', () => {
+      const slidesToShow = 3;
       const params = {
         ...sharedParams,
-        slidesToShow: 3
-      };
-
-      const swipeSlider = (backward) => {
-        cy.get('.slider-container')
-          .trigger('mousedown', { which: 1 })
-          .trigger('mousemove', {
-            clientX: backward ? 0 : 500
-          })
-          .trigger('mousemove', {
-            clientX: backward ? 500 : 0
-          })
-          .trigger('mouseup');
+        slidesToShow
       };
 
       cy.visit(
         `http://localhost:3000/?slides=6&params=${JSON.stringify(params)}`
       );
 
-      swipeSlider();
+      // Half of one slide should be barely enough to scroll once with the
+      // default threshold
+      const distance = carouselWidth / slidesToShow / 2;
 
-      cy.get('.slide.slide-visible')
-        .should('have.length', 3)
-        .find('img')
-        .first()
-        .should('have.attr', 'data-slide', 'Slide 2');
+      assertFirstSlideIs(1);
 
-      swipeSlider();
+      swipeSlider(distance);
 
-      cy.get('.slide.slide-visible')
-        .should('have.length', 3)
-        .find('img')
-        .first()
-        .should('have.attr', 'data-slide', 'Slide 3');
+      assertFirstSlideIs(2);
 
-      swipeSlider();
+      swipeSlider(distance);
 
-      cy.get('.slide.slide-visible')
-        .should('have.length', 3)
-        .find('img')
-        .first()
-        .should('have.attr', 'data-slide', 'Slide 4');
+      assertFirstSlideIs(3);
 
-      swipeSlider(true);
+      swipeSlider(-distance);
 
-      cy.get('.slide.slide-visible')
-        .should('have.length', 3)
-        .find('img')
-        .first()
-        .should('have.attr', 'data-slide', 'Slide 3');
+      assertFirstSlideIs(2);
 
-      swipeSlider(true);
+      swipeSlider(-distance);
 
-      cy.get('.slide.slide-visible')
-        .should('have.length', 3)
-        .find('img')
-        .first()
-        .should('have.attr', 'data-slide', 'Slide 2');
-
-      swipeSlider(true);
-
-      cy.get('.slide.slide-visible')
-        .should('have.length', 3)
-        .find('img')
-        .first()
-        .should('have.attr', 'data-slide', 'Slide 1');
+      assertFirstSlideIs(1);
     });
 
     it('should render a carousel with 6 slides and 3 visible slides and not advance with a small swipe', () => {
+      const slidesToShow = 3;
       const params = {
         ...sharedParams,
-        slidesToShow: 3
+        slidesToShow
       };
 
-      const swipeSlider = (backward) => {
-        cy.get('.slider-container')
-          .trigger('mousedown', { which: 1 })
-          .trigger('mousemove', {
-            clientX: backward ? 0 : 50
-          })
-          .trigger('mousemove', {
-            clientX: backward ? 50 : 0
-          })
-          .trigger('mouseup');
-      };
+      // Half of one slide should be barely enough to scroll once with the
+      // default threshold
+      const distance = carouselWidth / slidesToShow / 2 - 1;
 
       cy.visit(
         `http://localhost:3000/?slides=6&params=${JSON.stringify(params)}`
       );
 
-      swipeSlider();
+      assertFirstSlideIs(1);
 
-      cy.get('.slide.slide-visible')
-        .should('have.length', 3)
-        .find('img')
-        .first()
-        .should('have.attr', 'data-slide', 'Slide 1');
+      swipeSlider(distance);
 
-      swipeSlider(true);
+      assertFirstSlideIs(1);
 
-      cy.get('.slide.slide-visible')
-        .should('have.length', 3)
-        .find('img')
-        .first()
-        .should('have.attr', 'data-slide', 'Slide 1');
+      swipeSlider(distance + 1);
+
+      assertFirstSlideIs(2);
+
+      swipeSlider(-distance);
+
+      assertFirstSlideIs(2);
     });
   });
 });
