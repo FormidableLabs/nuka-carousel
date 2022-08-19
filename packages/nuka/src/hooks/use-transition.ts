2a728d82ea42b74f91acc2f8f755b73c90a6af0d
Shane Buchan <shane@buchan.io>
2022-08-19 10:09:20 -0400

    Replaced `d3-ease` with `victory-vendor` to fix SSR compilation errors

--- packages/nuka/src/hooks/use-transition.ts
+++ packages/nuka/src/hooks/use-transition.ts
@@ -1,6 +1,6 @@
 import { useEffect, useState, useRef } from 'react';
 import { D3EasingFunctions } from 'src/types';
-import * as d3 from 'd3-ease';
+import * as d3Ease from 'victory-vendor/d3-ease';
 
 export const useTransition = (
   duration: number, // in milliseconds
@@ -40,5 +40,5 @@ export const useTransition = (
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, deps);
 
-  return d3[animationTimingFunction](normalizedTime);
+  return d3Ease[animationTimingFunction](normalizedTime);
 };
