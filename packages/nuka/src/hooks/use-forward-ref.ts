7008a4edcca4820e6c93851584f033ebc8191be2
Jeehyae Dance <jeehyaedance@Jeehyaes-MacBook-Pro.local>
2022-10-18 22:15:06 +0900

    create use forwardref hook to work around with React.forwardRef

---
+++ packages/nuka/src/hooks/use-forward-ref.ts
@@ -0,0 +1,20 @@
+import { useEffect, useRef, ForwardedRef } from 'react';
+
+export const useForwardRef = <T>(
+  ref: ForwardedRef<T>,
+  initialValue: any = null
+) => {
+  const targetRef = useRef<T>(initialValue);
+
+  useEffect(() => {
+    if (!ref) return;
+
+    if (typeof ref === 'function') {
+      ref(targetRef.current);
+    } else {
+      ref.current = targetRef.current;
+    }
+  }, [ref]);
+
+  return targetRef;
+};
