3ff14716083a4aa7efad29abf82cafa5c2660744
Andrew <andrew@isfluent.com>
2022-11-15 08:39:10 +0000

    fix: tabIndex changes from review; changeset info

--- packages/nuka/src/carousel.tsx
+++ packages/nuka/src/carousel.tsx
@@ -646,7 +646,7 @@ export const Carousel = (rawProps: CarouselProps): React.ReactElement => {
         }}
         aria-label={frameAriaLabel}
         role="region"
-        tabIndex={enableKeyboardControls ? 0 : undefined}
+        tabIndex={enableKeyboardControls ? 0 : -1}
         onKeyDown={enableKeyboardControls ? onKeyDown : undefined}
         ref={carouselRef}
         onMouseUp={onMouseUp}
