528c28f11dc1b749b3e9f30f2ac121ba627b5c57
ally palanzi <ampalanzi@gmail.com>
2023-03-13 17:24:25 -0500

    fix(inert): only include inert property when it is true

--- packages/nuka/src/slide.tsx
+++ packages/nuka/src/slide.tsx
@@ -152,7 +152,7 @@ const Slide = ({
   return (
     <div
       ref={slideRef}
-      {...{ inert: (!isFullyVisible).toString() }}
+      {...{ inert: isFullyVisible ? undefined : 'true' }}
       className={`slide${currentSlideClass}${
         typeOfSlide ? ` ${typeOfSlide}` : ''
       }${isFullyVisible ? ' slide-visible' : ''}`}
