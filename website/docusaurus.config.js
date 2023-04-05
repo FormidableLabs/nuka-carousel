7a31c27b57438a57ab135f1f1268ceadb242fd78
Carlos Kelly <me@carlos.dev>
2023-04-05 16:27:50 -0500

    Fix base url

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: 'Nuka Carousel',
   tagline: 'A flexible playground for live editing React components',
   url: 'https://formidable.com',
-  baseUrl: '/open-source/nuka-carousel',
+  baseUrl: '',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
