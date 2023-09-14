f21227999fae0966ad07ba2398741f543a157540
Clark Gunn <clark.gunn@formidable.com>
2023-09-14 12:24:18 -0400

    Add wait and additional assertions to cypress test.

--- packages/nuka/cypress/integration/carousel.spec.ts
+++ packages/nuka/cypress/integration/carousel.spec.ts
@@ -1,15 +1,17 @@
 /* eslint-disable cypress/no-unnecessary-waiting */
+const waitThreshold = 100;
 
 context('Carousel', () => {
   // Speed up transition animation time
   const sharedParams = { speed: 50 };
 
   describe('Carousel variations', () => {
-    it.skip('should render carousel with 4 slides and only 1 visible slide and go through all of the slides', () => {
+    it('should render carousel with 4 slides and only 1 visible slide and go through all of the slides', () => {
       cy.visitWithCarouselProps({
         ...sharedParams,
         slideCount: 4,
       });
+      cy.wait(waitThreshold);
 
       cy.get('.slide.slide-visible')
         .should('have.length', 1)
@@ -23,6 +25,7 @@ context('Carousel', () => {
 
       cy.get('button').contains('Prev').should('be.disabled');
       cy.get('button').contains('Next').should('not.be.disabled').click();
+      cy.wait(waitThreshold);
 
       cy.get('.slide.slide-visible')
         .should('have.length', 1)
@@ -31,6 +34,7 @@ context('Carousel', () => {
 
       cy.get('button').contains('Prev').should('not.be.disabled');
       cy.get('button').contains('Next').should('not.be.disabled').click();
+      cy.wait(waitThreshold);
 
       cy.get('.slide.slide-visible')
         .should('have.length', 1)
@@ -39,6 +43,7 @@ context('Carousel', () => {
 
       cy.get('button').contains('Prev').should('not.be.disabled');
       cy.get('button').contains('Next').should('not.be.disabled').click();
+      cy.wait(waitThreshold);
 
       cy.get('.slide.slide-visible')
         .should('have.length', 1)
@@ -47,6 +52,7 @@ context('Carousel', () => {
 
       cy.get('button').contains('Next').should('be.disabled');
       cy.get('button').contains('Prev').should('not.be.disabled').click();
+      cy.wait(waitThreshold);
 
       cy.get('.slide.slide-visible')
         .should('have.length', 1)
@@ -57,13 +63,14 @@ context('Carousel', () => {
       cy.get('button').contains('Next').should('not.be.disabled');
     });
 
-    it.skip('should render carousel with 6 slides and 2 visible slides and slides to scroll equal to 2', () => {
+    it('should render carousel with 6 slides and 2 visible slides and slides to scroll equal to 2', () => {
       cy.visitWithCarouselProps({
         ...sharedParams,
         slideCount: 6,
         slidesToShow: 2,
         slidesToScroll: 2,
       });
+      cy.wait(waitThreshold);
 
       cy.get('.slide.slide-visible')
         .should('have.length', 2)
@@ -73,6 +80,7 @@ context('Carousel', () => {
 
       cy.get('button').contains('Prev').should('be.disabled');
       cy.get('button').contains('Next').should('not.be.disabled').click();
+      cy.wait(waitThreshold);
 
       cy.get('.slide.slide-visible')
         .should('have.length', 2)
@@ -82,6 +90,7 @@ context('Carousel', () => {
 
       cy.get('button').contains('Prev').should('not.be.disabled');
       cy.get('button').contains('Next').should('not.be.disabled').click();
+      cy.wait(waitThreshold);
 
       cy.get('.slide.slide-visible')
         .should('have.length', 2)
@@ -100,6 +109,7 @@ context('Carousel', () => {
         slidesToShow: 2,
         withoutControls: true,
       });
+      cy.wait(waitThreshold);
 
       cy.get('.slide.slide-visible')
         .should('have.length', 2)
@@ -170,7 +180,8 @@ context('Carousel', () => {
       cy.get('.paging-item')
         .first()
         .parent()
-        .should('have.attr', 'role', 'tablist');
+        .should('have.attr', 'role', 'tablist')
+        .and('have.attr', 'aria-label', 'Choose slide to display.');
       cy.get(`#${params.carouselId}-slide-1`).should(
         'have.attr',
         'role',
@@ -182,7 +193,8 @@ context('Carousel', () => {
         'aria-roledescription'
       );
 
-      cy.get('[aria-label="next"]').click();
+      cy.get('button').contains('Next').click();
+      cy.wait(waitThreshold);
 
       cy.get('.paging-item')
         .eq(0)
