250301742838922813d449808e7a231e2d27a2bb
Carlos Kelly <me@carlos.dev>
2023-10-12 09:56:13 -0500

    Fix storybook configuration

--- packages/nuka/.storybook/main.js
+++ packages/nuka/.storybook/main.js
@@ -3,9 +3,15 @@ module.exports = {
     '../stories/**/*.stories.mdx',
     '../stories/**/*.stories.@(js|jsx|ts|tsx)',
   ],
+
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
-  framework: '@storybook/react',
-  core: {
-    builder: 'webpack5',
+
+  framework: {
+    name: '@storybook/react-webpack5',
+    options: {}
   },
+
+  docs: {
+    autodocs: true
+  }
 };
