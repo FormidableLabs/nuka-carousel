f58d2d2dd7e15c2ab0cc626e1c710880e9a0a767
Carlos Kelly <me@carlos.dev>
2023-11-01 12:13:25 -0500

    Update cypress.config.ts

--- packages/nuka/cypress.config.ts
+++ packages/nuka/cypress.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'cypress'
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
   retries: 3,
@@ -6,7 +6,7 @@ export default defineConfig({
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.ts')(on, config)
+      return require('./cypress/plugins/index.ts')(on, config);
     },
   },
-})
+});
