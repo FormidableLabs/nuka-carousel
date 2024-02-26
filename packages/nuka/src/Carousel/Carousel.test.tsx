50e3aabc14cc7a1d2b7421f5d444a0e2617deccd
Melvin Chen <melvin.chen@formidable.com>
2024-02-26 12:10:58 -0800

    Update test script for storybook testing, comment out cypress tests

--- packages/nuka/src/Carousel/Carousel.test.tsx
+++ packages/nuka/src/Carousel/Carousel.test.tsx
@@ -2,26 +2,26 @@
  * @jest-environment jsdom
  */
 
-import { render } from '@testing-library/react';
-import { Carousel } from './Carousel';
-import { ExampleSlide } from './ExampleSlide';
+// import { render } from '@testing-library/react';
+// import { Carousel } from './Carousel';
+// import { ExampleSlide } from './ExampleSlide';
 
 describe('Carousel', () => {
-  it('renders', () => {
-    render(
-      <Carousel>
-        <ExampleSlide index={0} />
-      </Carousel>
-    );
-  });
-  it.todo('passes a className and ref for the parent');
-  it.todo('passes the same className for each of the children');
+  // it('renders', () => {
+  //   render(
+  //     <Carousel>
+  //       <ExampleSlide index={0} />
+  //     </Carousel>
+  //   );
+  // });
+  // it.todo('passes a className and ref for the parent');
+  // it.todo('passes the same className for each of the children');
   it.todo('is draggable through touch controls');
-  it.todo('has a next and previous function that is callable from its parent');
-  it.todo('can slide');
+  // it.todo('has a next and previous function that is callable from its parent');
+  // it.todo('can slide');
   it.todo('omits slides whose children are falsy');
 
-  it.todo('autoplays at the right rate');
+  // it.todo('autoplays at the right rate');
 
   it.todo('returns the right number of page dots');
 });
