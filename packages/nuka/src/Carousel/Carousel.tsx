9ad5c30567f6d3051be3294f991cfa24f13a5206
Charlie Brown <carbonrobot@gmail.com>
2024-03-16 09:58:46 -0500

    Refactor measurements into hook

--- packages/nuka/src/Carousel/Carousel.tsx
+++ packages/nuka/src/Carousel/Carousel.tsx
@@ -5,7 +5,6 @@ import {
   useEffect,
   useImperativeHandle,
   useRef,
-  useState,
 } from 'react';
 
 import './Carousel.css';
@@ -13,7 +12,8 @@ import { PageIndicators } from '../PageIndicators/PageIndicators';
 import { useInterval } from 'src/hooks/use-interval';
 import { usePaging } from 'src/hooks/use-paging';
 import { useDebounced } from 'src/hooks/use-debounced';
-import { arraySeq, arraySum, nint } from 'src/utils';
+import { useMeasurement } from 'src/hooks/use-measurement';
+import { nint } from 'src/utils';
 
 type ScrollDistanceType = number | 'slide' | 'screen';
 
@@ -63,63 +63,20 @@ export const Carousel = forwardRef<SlideHandle, CarouselProps>(
     const containerRef = useRef<HTMLDivElement | null>(null);
     const wrapperRef = useRef<HTMLDivElement | null>(null);
 
+    // -- update page count and scroll offset based on scroll distance
     const totalSlides = Children.count(children);
-    const [totalPages, setTotalPages] = useState(totalSlides);
-    const [scrollOffset, setScrollOffset] = useState(arraySeq(totalPages, 0));
+    const { totalPages, scrollOffset } = useMeasurement({
+      totalSlides,
+      scrollDistance,
+      containerRef,
+      wrapperRef,
+    });
+
+    // -- paging
     const { currentPage, goBack, goForward, goToPage } = usePaging(
       totalPages,
       wrapAround
     );
-
-    // -- update page count and scroll offset based on scroll distance
-    useEffect(() => {
-      // execute before paint to ensure refs are set with the
-      // correct dimensions for the calculation
-      // note: this is similar to useLayout, but runs async
-      requestAnimationFrame(() => {
-        switch (scrollDistance) {
-          case 'screen': {
-            if (containerRef.current && wrapperRef.current) {
-              const pageCount = Math.ceil(
-                wrapperRef.current.scrollWidth /
-                  containerRef.current.offsetWidth
-              );
-              setTotalPages(pageCount);
-              setScrollOffset(
-                arraySeq(pageCount, containerRef.current.offsetWidth)
-              );
-            }
-            break;
-          }
-          case 'slide': {
-            if (wrapperRef.current) {
-              // creates an array of slide widths in order to support
-              // slides of varying widths as children
-              const offsets = Array.from(wrapperRef.current.children).map(
-                (child) => (child as HTMLElement).offsetWidth
-              );
-
-              setTotalPages(totalSlides);
-              setScrollOffset(arraySum([0, ...offsets.slice(0, -1)]));
-            }
-            break;
-          }
-          default: {
-            if (containerRef.current && typeof scrollDistance === 'number') {
-              const carouselTotalWidth =
-                containerRef.current.scrollWidth -
-                containerRef.current.offsetWidth;
-
-              const pageCount =
-                Math.ceil(carouselTotalWidth / scrollDistance) + 1;
-
-              setTotalPages(pageCount);
-              setScrollOffset(arraySeq(pageCount, scrollDistance));
-            }
-          }
-        }
-      });
-    }, [scrollDistance, totalSlides]);
 
     // -- autoplay
     useInterval(goForward, autoplayInterval, autoplay);
---
+++ packages/nuka/src/hooks/use-measurement.tsx
@@ -0,0 +1,70 @@
+import { useEffect, useState } from 'react';
+
+import { arraySeq, arraySum } from 'src/utils';
+
+type MeasurementProps = {
+  totalSlides: number;
+  scrollDistance: number | 'slide' | 'screen';
+  containerRef: React.RefObject<HTMLDivElement>;
+  wrapperRef: React.RefObject<HTMLDivElement>;
+};
+
+export function useMeasurement({
+  totalSlides,
+  scrollDistance,
+  containerRef,
+  wrapperRef,
+}: MeasurementProps) {
+  const [totalPages, setTotalPages] = useState(totalSlides);
+  const [scrollOffset, setScrollOffset] = useState(arraySeq(totalPages, 0));
+
+  useEffect(() => {
+    // execute before paint to ensure refs are set with the
+    // correct dimensions for the calculation
+    // note: this is similar to useLayout, but runs async
+    requestAnimationFrame(() => {
+      switch (scrollDistance) {
+        case 'screen': {
+          if (containerRef.current && wrapperRef.current) {
+            const pageCount = Math.ceil(
+              wrapperRef.current.scrollWidth / containerRef.current.offsetWidth
+            );
+            setTotalPages(pageCount);
+            setScrollOffset(
+              arraySeq(pageCount, containerRef.current.offsetWidth)
+            );
+          }
+          break;
+        }
+        case 'slide': {
+          if (wrapperRef.current) {
+            // creates an array of slide widths in order to support
+            // slides of varying widths as children
+            const offsets = Array.from(wrapperRef.current.children).map(
+              (child) => (child as HTMLElement).offsetWidth
+            );
+
+            setTotalPages(totalSlides);
+            setScrollOffset(arraySum([0, ...offsets.slice(0, -1)]));
+          }
+          break;
+        }
+        default: {
+          if (containerRef.current && typeof scrollDistance === 'number') {
+            const carouselTotalWidth =
+              containerRef.current.scrollWidth -
+              containerRef.current.offsetWidth;
+
+            const pageCount =
+              Math.ceil(carouselTotalWidth / scrollDistance) + 1;
+
+            setTotalPages(pageCount);
+            setScrollOffset(arraySeq(pageCount, scrollDistance));
+          }
+        }
+      }
+    });
+  }, [scrollDistance, totalSlides, containerRef, wrapperRef]);
+
+  return { totalPages, scrollOffset };
+}
