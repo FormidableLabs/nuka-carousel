4bbfa396011ac3af10ae7d791a085aacba061bc3
Charlie Brown <carbonrobot@gmail.com>
2024-03-16 07:30:13 -0500

    Create default page indicator styling

--- packages/nuka/src/PageIndicators/PageIndicators.tsx
+++ packages/nuka/src/PageIndicators/PageIndicators.tsx
@@ -16,18 +16,26 @@ export const PageIndicators = ({
   scrollToPage,
 }: PageIndicatorsProps) => {
   return (
-    <div className={containerClassName} data-testid="pageIndicatorContainer">
+    <div
+      className={containerClassName || 'nuka-page-container'}
+      data-testid="pageIndicatorContainer"
+    >
       {[...Array(totalIndicators)].map((_, index) => (
         <button
           key={index}
           onClick={() => scrollToPage(index)}
-          className={
-            pageIndicatorClassName +
-            ' ' +
-            (currentPageIndex === index ? currentPageIndicatorClassName : '')
-          }
+          className={classNames(
+            pageIndicatorClassName || 'nuka-page-indicator',
+            currentPageIndex === index
+              ? currentPageIndicatorClassName || 'active'
+              : ''
+          )}
         />
       ))}
     </div>
   );
 };
+
+function classNames(...classes: string[]) {
+  return classes.filter(Boolean).join(' ');
+}
